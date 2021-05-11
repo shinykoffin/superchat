73dc02aba13f621f67ecb7348032e38bfa8c391e
Luis Luna <luisluna.am@gmail.com>
2021-05-11 02:30:19 -0500

    Added deleted function and button

--- src/Components/ChatMessage.jsx
+++ src/Components/ChatMessage.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
-import { auth }  from '../FireConfig/FirebaseConect'
+import { auth, firestore }  from '../FireConfig/FirebaseConect'
+
 
 function ChatMessage(props) {
-  const { text, uid, photoURL} = props.message;
+  const { text, uid, photoURL, id} = props.message;
   const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+
+  const deleteMessage = (e)=>{
+    e.preventDefault();
+    firestore.collection("messages").doc(id).delete()
+      .then(()=>{
+        console.log("Message deleted");
+      })
+      .catch(err => console.log(err))
+  }
 
   return (
     <div className={`message ${messageClass}`}>
+      {(uid === auth.currentUser.uid) && <button onClick={deleteMessage}>Delete</button>}
       <img className="profilePicture" src={photoURL} />
       <p className="messageText">{text}</p>
     </div>
